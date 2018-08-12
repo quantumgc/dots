f0d42f3bdea1224c52b27b67be125db028ecbe13
Gautam Chaudhuri <14055370+QuantumAnon@users.noreply.github.com>
2018-08-12 17:34:53 +0100

    removed compton launch script

--- compton/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Terminate already running bar instances
-killall -q compton
-
-# Wait until the processes have been shut down
-while pgrep -x compton >/dev/null; do sleep 1; done
-
-compton --config $HOME/.config/compton/compton.conf
